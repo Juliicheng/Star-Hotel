b671819d992d2ff533289b7ee17cdeb196033e41
Ding <anluoridge@gmail.com>
2017-10-02 00:42:08 +1100

    combine controller and fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button okButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.okButton.setOnAction(
+                event -> System.out.println("Touch OK") // Lambda
+        /* old version
+        new EventHandler<>() {
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+    public void handle(ActionEvent event) {
+        System.out.println("Touch OK");
+    }
+}*/
+        );
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,41 +1,31 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+// import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 import java.util.*;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        System.out.println("Test...\nType anything:");
-        Scanner scanner = new Scanner(System.in);
-        String testString = scanner.next();
-        System.out.println(testString);
+    public void start(Stage primaryStage) throws Exception {
 
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(
-                event -> System.out.println("Hello World!")
-//                new EventHandler<>() {
-//                    btn.setOnAction(new EventHandler<ActionEvent>() {
-//                @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        }
-        );
+//        System.out.println("Test...\nType anything:");
+//        Scanner scanner = new Scanner(System.in);
+//        String testString = scanner.next();
+//        System.out.println(testString);
+//
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        Scene scene = new Scene(root, 300, 250);
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/sample.fxml"));
 
-        primaryStage.setTitle("Hello World!");
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setTitle("Reservation");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
